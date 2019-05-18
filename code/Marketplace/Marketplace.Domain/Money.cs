75bebb9302709c91fe69e5988cf7e6aad7a0a9ca
Patrick Drechsler <socialcoding@pdrechsler.de>
2019-05-18 20:56:05 +0200

    add domain service CurrencyLookup

--- code/Marketplace/Marketplace.Domain/Money.cs
+++ code/Marketplace/Marketplace.Domain/Money.cs
@@ -7,39 +7,53 @@ namespace Marketplace.Domain
     {
         private const string DefaultCurrency = "EUR";
 
-        protected Money(decimal amount, string currency = "EUR")
+        protected Money(decimal amount, string currencyCode, ICurrencyLookup currencyLookup)
         {
-            if (decimal.Round(amount, 2) != amount)
+            if (string.IsNullOrEmpty(currencyCode))
+                throw new ArgumentNullException(nameof(currencyCode), 
+                    "Currency code must be specified");
+
+            var currency = currencyLookup.FindCurrency(currencyCode);
+            if (!currency.InUse)
+                throw new ArgumentException($"Currency {currencyCode} is not valid");
+
+            if (decimal.Round(amount, currency.DecimalPlaces) != amount)
                 throw new ArgumentOutOfRangeException(nameof(amount),
-                    "Amount cannot have more than two decimals");
+                    $"Amount in {currencyCode} cannot have more than {currency.DecimalPlaces} decimals");
 
             Amount = amount;
-            CurrencyCode = currency;
+            Currency = currency;
+        }
+
+        private Money(decimal amount, CurrencyDetails currency)
+        {
+            Amount = amount;
+            Currency = currency;
         }
 
         public decimal Amount { get; }
-        public string CurrencyCode { get; }
+        public CurrencyDetails Currency { get; }
 
-        public static Money FromDecimal(decimal amount, string currency = DefaultCurrency) 
-            => new Money(amount, currency);
+        public static Money FromDecimal(decimal amount, string currency, ICurrencyLookup currencyLookup) 
+            => new Money(amount, currency, currencyLookup);
 
-        public static Money FromString(string amount, string currency = DefaultCurrency) 
-            => new Money(decimal.Parse(amount), currency);
+        public static Money FromString(string amount, string currency, ICurrencyLookup currencyLookup) 
+            => new Money(decimal.Parse(amount), currency, currencyLookup);
 
         public Money Add(Money summand)
         {
-            if (CurrencyCode != summand.CurrencyCode)
+            if (Currency != summand.Currency)
                 throw new CurrencyMismatchException("Cannot sum amounts with different currencies");
 
-            return new Money(Amount + summand.Amount, CurrencyCode);
+            return new Money(Amount + summand.Amount, Currency);
         }
 
         public Money Subtract(Money subtrahend)
         {
-            if (CurrencyCode != subtrahend.CurrencyCode)
+            if (Currency != subtrahend.Currency)
                 throw new CurrencyMismatchException("Cannot subtract amounts with different currencies");
 
-            return new Money(Amount - subtrahend.Amount, CurrencyCode);
+            return new Money(Amount - subtrahend.Amount, Currency);
         }
 
         public static Money operator +(Money summand1, Money summand2) => summand1.Add(summand2);
--- code/Marketplace/Marketplace.Domain/Price.cs
+++ code/Marketplace/Marketplace.Domain/Price.cs
@@ -2,13 +2,13 @@ using System;
 
 namespace Marketplace.Domain
 {
-    public class Price : Money
-    {
-        public Price(decimal amount) : base(amount)
-        {
-            if (amount < 0)
-                throw new ArgumentException(
-                    "Price cannot be negativ", nameof(amount));
-        }
-    }
+//    public class Price : Money
+//    {
+//        public Price(decimal amount) : base(amount)
+//        {
+//            if (amount < 0)
+//                throw new ArgumentException(
+//                    "Price cannot be negativ", nameof(amount));
+//        }
+//    }
 }
