038f4da5ef300c73e976498b9419027c375d0831
Patrick Drechsler <socialcoding@pdrechsler.de>
2019-05-18 19:56:45 +0200

    Money: add currency as string

--- code/Marketplace/Marketplace.Domain/Money.cs
+++ code/Marketplace/Marketplace.Domain/Money.cs
@@ -5,26 +5,52 @@ namespace Marketplace.Domain
 {
     public class Money : Value<Money>
     {
-        public decimal Amount { get; }
+        private const string DefaultCurrency = "EUR";
 
-        public static Money FromDecimal(decimal amount) => new Money(amount);
-        
-        protected Money(decimal amount)
+        protected Money(decimal amount, string currency = "EUR")
         {
             if (decimal.Round(amount, 2) != amount)
-            {
                 throw new ArgumentOutOfRangeException(nameof(amount),
                     "Amount cannot have more than two decimals");
-            }
+
             Amount = amount;
+            CurrencyCode = currency;
         }
 
-        public Money Add(Money summand) => new Money(Amount + summand.Amount);
-        
-        public Money Subtract(Money subtrahend) => new Money(Amount - subtrahend.Amount);
+        public decimal Amount { get; }
+        public string CurrencyCode { get; }
+
+        public static Money FromDecimal(decimal amount, string currency = DefaultCurrency) 
+            => new Money(amount, currency);
+
+        public static Money FromString(string amount, string currency = DefaultCurrency) 
+            => new Money(decimal.Parse(amount), currency);
+
+        public Money Add(Money summand)
+        {
+            if (CurrencyCode != summand.CurrencyCode)
+                throw new CurrencyMismatchException("Cannot sum amounts with different currencies");
+
+            return new Money(Amount + summand.Amount, CurrencyCode);
+        }
+
+        public Money Subtract(Money subtrahend)
+        {
+            if (CurrencyCode != subtrahend.CurrencyCode)
+                throw new CurrencyMismatchException("Cannot subtract amounts with different currencies");
+
+            return new Money(Amount - subtrahend.Amount, CurrencyCode);
+        }
 
         public static Money operator +(Money summand1, Money summand2) => summand1.Add(summand2);
-        
+
         public static Money operator -(Money minuend, Money subtrahend) => minuend.Subtract(subtrahend);
+    }
+
+    public class CurrencyMismatchException : Exception
+    {
+        public CurrencyMismatchException(string message) : base(message)
+        {
+        }
     }
 }
--- code/Marketplace/Marketplace.Tests/MoneyTest.cs
+++ code/Marketplace/Marketplace.Tests/MoneyTest.cs
@@ -23,5 +23,14 @@ namespace Marketplace.Tests
             
             Assert.Equal(banknote, coin1 + coin2 + coin3);
         }
+
+        [Fact]
+        public void Sum_of_money_has_correct_currency()
+        {
+            var money1 = Money.FromDecimal(1, "USD");
+            var money2 = Money.FromDecimal(1, "USD");
+            
+            Assert.Equal(Money.FromDecimal(2, "USD"), money1 + money2);
+        }
     }
 }
