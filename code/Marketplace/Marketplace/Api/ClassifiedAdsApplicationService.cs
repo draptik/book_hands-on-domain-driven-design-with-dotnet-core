b31b3219ad27f1e59d8db021192f2f1ec3e76f95
Patrick Drechsler <socialcoding@pdrechsler.de>
2019-05-21 02:52:10 +0200

    introduce api service...

---
+++ code/Marketplace/Marketplace/Api/ClassifiedAdsApplicationService.cs
@@ -0,0 +1,10 @@
+namespace Marketplace.Api
+{
+    public class ClassifiedAdsApplicationService
+    {
+        public void Handle(Contracts.ClassifiedAds.V1.Create command)
+        {
+            // TODO
+        }
+    }
+}
