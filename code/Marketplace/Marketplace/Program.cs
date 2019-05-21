ba10fc6fdcd787cab6ea656499b84b2de97f76a0
Patrick Drechsler <socialcoding@pdrechsler.de>
2019-05-21 02:21:12 +0200

    cool: System.Environment.CurrentDirectory - didn't know that ;-)

--- code/Marketplace/Marketplace/Program.cs
+++ code/Marketplace/Marketplace/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using static System.Environment;
 using static System.Reflection.Assembly;
 
 
@@ -10,9 +11,8 @@ namespace Marketplace
     public static class Program
     {
         static Program()
+            // ReSharper disable once PossibleNullReferenceException
             => CurrentDirectory = Path.GetDirectoryName(GetEntryAssembly().Location);
-
-        private static string CurrentDirectory { get; }
 
         public static void Main(string[] args)
         {
