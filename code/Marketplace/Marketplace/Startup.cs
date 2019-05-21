aa0f582c2017a2670862b09d18678ff13c6e6049
Patrick Drechsler <socialcoding@pdrechsler.de>
2019-05-21 02:53:13 +0200

    add api service to IoC

--- code/Marketplace/Marketplace/Startup.cs
+++ code/Marketplace/Marketplace/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Marketplace.Api;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -24,6 +25,7 @@ namespace Marketplace
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(new ClassifiedAdsApplicationService());
             services.AddMvc();
             services.AddSwaggerGen(c =>
                 c.SwaggerDoc("v1",
