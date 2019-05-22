2f7bedcd653ac36cde9dda5454d71a648b91140f
Patrick Drechsler <socialcoding@pdrechsler.de>
2019-05-22 21:16:17 +0200

    application service: minimal event handler impl

--- code/Marketplace/Marketplace/Api/ClassifiedAdsApplicationService.cs
+++ code/Marketplace/Marketplace/Api/ClassifiedAdsApplicationService.cs
@@ -1,10 +1,42 @@
+using System;
+using System.Threading.Tasks;
+using Marketplace.Domain;
+using Marketplace.Framework;
+using static Marketplace.Contracts.ClassifiedAds;
+
 namespace Marketplace.Api
 {
-    public class ClassifiedAdsApplicationService
+    public class ClassifiedAdsApplicationService : IApplicationService
     {
-        public void Handle(Contracts.ClassifiedAds.V1.Create command)
+        private readonly IClassifiedAdRepository _repository;
+        private readonly ICurrencyLookup _currencyLookup;
+
+        public ClassifiedAdsApplicationService(
+            IClassifiedAdRepository repository,
+            ICurrencyLookup currencyLookup)
         {
-            // TODO
+            _repository = repository;
+            _currencyLookup = currencyLookup;
+        }
+
+        public Task Handle(object command) =>
+            command switch
+                {
+                V1.Create cmd => HandleCreate(cmd),
+                _ => Task.CompletedTask
+                };
+
+        private async Task HandleCreate(V1.Create cmd)
+        {
+            if (await _repository.Exists(cmd.Id.ToString()))
+                throw new InvalidOperationException($"Entity with id {cmd.Id} already exists");
+
+            var classifiedAd = new ClassifiedAd(
+                new ClassifiedAdId(cmd.Id),
+                new UserId(cmd.OwnerId)
+            );
+
+            await _repository.Save(classifiedAd);
         }
     }
 }
