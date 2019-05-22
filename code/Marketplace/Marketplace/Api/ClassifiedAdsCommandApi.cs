98310ea7680ee5496862c129190553c29fac2e98
Patrick Drechsler <socialcoding@pdrechsler.de>
2019-05-22 21:14:36 +0200

    api: use interface

--- code/Marketplace/Marketplace/Api/ClassifiedAdsCommandApi.cs
+++ code/Marketplace/Marketplace/Api/ClassifiedAdsCommandApi.cs
@@ -1,16 +1,21 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using static Marketplace.Contracts.ClassifiedAds;
 
 namespace Marketplace.Api
 {
     [Route("/ad")]
     public class ClassifiedAdsCommandApi : Controller
     {
+        private readonly ClassifiedAdsApplicationService _applicationService;
+
+        public ClassifiedAdsCommandApi(ClassifiedAdsApplicationService applicationService) 
+            => _applicationService = applicationService;
+
         [HttpPost]
-        public async Task<IActionResult> Post(
-            Contracts.ClassifiedAds.V1.Create request)
+        public async Task<IActionResult> Post(V1.Create request)
         {
-            // handle Request here
+            await _applicationService.Handle(request);
             return Ok();
         }
     }
