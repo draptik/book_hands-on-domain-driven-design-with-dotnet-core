dfc7acd0ceba5dd35df8a6fa0eff9b77aacfe342
Patrick Drechsler <socialcoding@pdrechsler.de>
2019-05-25 22:08:46 +0200

    chapter 9: add protected ctor

--- code/Marketplace/Marketplace.Domain/ClassifiedAdId.cs
+++ code/Marketplace/Marketplace.Domain/ClassifiedAdId.cs
@@ -5,15 +5,24 @@ namespace Marketplace.Domain
 {
     public class ClassifiedAdId : Value<ClassifiedAdId>
     {
-        private readonly Guid _value;
+        public Guid Value { get; }
 
-        public ClassifiedAdId(Guid value) => _value = value;
+        public ClassifiedAdId(Guid value)
+        {
+            if (value == default)
+                throw new ArgumentNullException(nameof(value), "Classified Ad id cannot be empty");
+
+            Value = value;
+        }
+
+        public static implicit operator Guid(ClassifiedAdId self) => self.Value;
         
-        public static implicit operator Guid(ClassifiedAdId self) => self._value;
-        
-        public override string ToString() => _value.ToString();
+        public override string ToString() => Value.ToString();
         
         public static implicit operator ClassifiedAdId(string value) 
             => new ClassifiedAdId(Guid.Parse(value));
+        
+        // Serialization requirement
+        protected ClassifiedAdId() { }
     }
 }
