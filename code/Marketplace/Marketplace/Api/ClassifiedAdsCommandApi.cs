67874ca312a533ed758c1554318df2d2ae6b02d7
Patrick Drechsler <socialcoding@pdrechsler.de>
2019-05-25 15:41:13 +0200

    chapter 8: helper to format failure output - add Serilog

--- code/Marketplace/Marketplace/Api/ClassifiedAdsCommandApi.cs
+++ code/Marketplace/Marketplace/Api/ClassifiedAdsCommandApi.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using static Marketplace.Contracts.ClassifiedAds;
+using Serilog;
 
 namespace Marketplace.Api
 {
@@ -49,6 +51,21 @@ namespace Marketplace.Api
         {
             await _applicationService.Handle(request);
             return Ok();
+        }
+
+        private async Task<IActionResult> HandleRequest<T>(T request, Func<T, Task> handler)
+        {
+            try
+            {
+                Log.Debug("Handling HTTP request of type{type}", typeof(T).Name);
+                await handler(request);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Log.Error("Error handling the request", e);
+                return new BadRequestObjectResult(new {error = e.Message, stackTrace = e.StackTrace});
+            }
         }
     }
 }
