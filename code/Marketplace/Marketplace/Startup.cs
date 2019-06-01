915a16d5284929882bc0bfaff254b018f8f643df
Patrick Drechsler <socialcoding@pdrechsler.de>
2019-06-01 22:27:24 +0200

    chapter 9 (ravendb): brocken

--- code/Marketplace/Marketplace/Startup.cs
+++ code/Marketplace/Marketplace/Startup.cs
@@ -1,13 +1,15 @@
 ﻿using Marketplace.ClassifiedAd;
 using Marketplace.Domain.ClassifiedAd;
 using Marketplace.Domain.Shared;
+using Marketplace.Domain.UserProfile;
 using Marketplace.Framework;
 using Marketplace.Infrastructure;
+using Marketplace.UserProfile;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Raven.Client.Documents;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace Marketplace
@@ -25,39 +27,48 @@ namespace Marketplace
 
         public void ConfigureServices(IServiceCollection services)
         {
+            var purgomalumClient = new PurgomalumClient();
+            
             // RavenDb ======================================================
             // setup RavenDb
-//            var store = new DocumentStore
-//            {
-//                Urls = new[] {"http://localhost:8080"},
-//                Database = "Marketplace_Chapter8",
-//                Conventions =
-//                {
-//                    FindIdentityProperty = m => m.Name == "DbId"
-//                }
-//            };
-//            store.Initialize();
-//
-//            services.AddSingleton<ICurrencyLookup, FixedCurrencyLookup>();
-//            services.AddScoped(c => store.OpenAsyncSession());
-//            services.AddScoped<IUnitOfWork, RavenDbUnitOfWork>();
-//            services.AddScoped<IClassifiedAdRepository, ClassifiedAdRepository>();
-//            services.AddScoped<ClassifiedAdsApplicationService>();
+            var store = new DocumentStore
+            {
+                Urls = new[] {"http://localhost:8080"},
+                Database = "Marketplace_Chapter9",
+                Conventions =
+                {
+                    FindIdentityProperty = m => m.Name == "DbId"
+                }
+            };
+            store.Initialize();
+
+            services.AddSingleton<ICurrencyLookup, FixedCurrencyLookup>();
+            services.AddScoped(c => store.OpenAsyncSession());
+            services.AddScoped<IUnitOfWork, RavenDbUnitOfWork>();
+            services.AddScoped<IClassifiedAdRepository, ClassifiedAdRepository>();
+            services.AddScoped<IUserProfileRepository, UserProfileRepository>();
+            services.AddScoped<ClassifiedAdsApplicationService>();
+            services.AddScoped(c =>
+                new UserProfileApplicationService(
+                    c.GetService<IUserProfileRepository>(),
+                    c.GetService<IUnitOfWork>(),
+                    text => purgomalumClient.CheckForProfanity(text)
+                        .GetAwaiter().GetResult()));
             // End RavenDb ==================================================
             
             
             
             // EF-Core ======================================================
-            const string connectionString = "Host=localhost;Database=Marketplace_Chapter8;Username=ddd;Password=book";
-            services
-                .AddEntityFrameworkNpgsql()
-                .AddDbContext<ClassifiedAdDbContext>(
-                    options => options.UseNpgsql(connectionString));
-
-            services.AddSingleton<ICurrencyLookup, FixedCurrencyLookup>();
-            services.AddScoped<IUnitOfWork, EfCoreUnitOfWork>();
-            services.AddScoped<IClassifiedAdRepository, ClassifiedAdRepository>();
-            services.AddScoped<ClassifiedAdsApplicationService>();
+//            const string connectionString = "Host=localhost;Database=Marketplace_Chapter8;Username=ddd;Password=book";
+//            services
+//                .AddEntityFrameworkNpgsql()
+//                .AddDbContext<ClassifiedAdDbContext>(
+//                    options => options.UseNpgsql(connectionString));
+//
+//            services.AddSingleton<ICurrencyLookup, FixedCurrencyLookup>();
+//            services.AddScoped<IUnitOfWork, EfCoreUnitOfWork>();
+//            services.AddScoped<IClassifiedAdRepository, ClassifiedAdRepository>();
+//            services.AddScoped<ClassifiedAdsApplicationService>();
             // End EF-Core ==================================================
             
             
@@ -78,7 +89,7 @@ namespace Marketplace
                 app.UseDeveloperExceptionPage();
             }
 
-            app.EnsureDatabase(); // <- only for EF-Core!!
+//            app.EnsureDatabase(); // <- only for EF-Core!!
             
             app.UseMvcWithDefaultRoute();
             app.UseSwagger();
