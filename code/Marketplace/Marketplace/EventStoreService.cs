02ef9fadb1ec610d0c23a17a72ad3041fc0e529e
Patrick Drechsler <socialcoding@pdrechsler.de>
2019-06-13 18:32:56 +0200

    chapter 11 (in-memory): add newly created subscription to EventStoreService

--- code/Marketplace/Marketplace/EventStoreService.cs
+++ code/Marketplace/Marketplace/EventStoreService.cs
@@ -1,6 +1,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using EventStore.ClientAPI;
+using Marketplace.Infrastructure;
 using Microsoft.Extensions.Hosting;
 
 namespace Marketplace
@@ -8,14 +9,24 @@ namespace Marketplace
     public class EventStoreService : IHostedService
     {
         private readonly IEventStoreConnection _esConnection;
+        private readonly EsSubscription _subscription;
 
-        public EventStoreService(IEventStoreConnection esConnection) => _esConnection = esConnection;
+        public EventStoreService(IEventStoreConnection esConnection,
+            EsSubscription subscription)
+        {
+            _esConnection = esConnection;
+            _subscription = subscription;
+        }
 
-        public Task StartAsync(CancellationToken cancellationToken)
-            => _esConnection.ConnectAsync();
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await _esConnection.ConnectAsync();
+            _subscription.Start();
+        }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
+            _subscription.Stop();
             _esConnection.Close();
             return Task.CompletedTask;
         }
