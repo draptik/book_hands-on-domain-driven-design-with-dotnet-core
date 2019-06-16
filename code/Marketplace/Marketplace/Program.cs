af020fcdf9115b247b5804848282bfff895410f4
Patrick Drechsler <patrick@pdrechsler.de>
2019-06-17 00:54:33 +0200

    chapter 11: simplify Program.cs to conform with demo code

--- code/Marketplace/Marketplace/Program.cs
+++ code/Marketplace/Marketplace/Program.cs
@@ -1,43 +1,24 @@
-﻿using System.IO;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Serilog;
-using static System.Environment;
-using static System.Reflection.Assembly;
 
 namespace Marketplace
 {
     public static class Program
     {
-        static Program() =>
-            // ReSharper disable once PossibleNullReferenceException
-            CurrentDirectory = Path.GetDirectoryName(GetEntryAssembly().Location);
-
         public static void Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
                 .WriteTo.Console()
                 .CreateLogger();
-
-            var configuration = BuildConfiguration(args);
-
-            ConfigureWebHost(configuration).Build().Run();
+            
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        private static IConfiguration BuildConfiguration(string[] args)
-            => new ConfigurationBuilder()
-                .SetBasePath(CurrentDirectory)
-                .AddJsonFile("appsettings.json", false, false)
-                .Build();
-
-        private static IWebHostBuilder ConfigureWebHost(
-            IConfiguration configuration)
-            => new WebHostBuilder()
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseSerilog()
-                .UseStartup<Startup>()
-                .UseConfiguration(configuration)
-                .UseContentRoot(CurrentDirectory)
-                .UseKestrel();
+                .UseStartup<Startup>();
     }
 }
