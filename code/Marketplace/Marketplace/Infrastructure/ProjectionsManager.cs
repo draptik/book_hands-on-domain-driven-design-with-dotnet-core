83ef1f4cc46c9013bab45537d15cc58f0a7bce98
Patrick Drechsler <socialcoding@pdrechsler.de>
2019-06-16 22:14:00 +0200

    chapter 11 (in-memory): add dedicated projections

--- code/Marketplace/Marketplace/Infrastructure/ProjectionsManager.cs
+++ code/Marketplace/Marketplace/Infrastructure/ProjectionsManager.cs
@@ -1,10 +1,7 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using EventStore.ClientAPI;
 using Marketplace.ClassifiedAd;
-using Marketplace.Domain.ClassifiedAd;
 using Serilog.Events;
 using ILogger = Serilog.ILogger;
 
@@ -43,52 +40,8 @@ namespace Marketplace.Infrastructure
             var @event = resolvedEvent.Deserialize();
             Log.Debug("Projecting event {type}", @event.GetType().Name);
 
-            switch (@event)
-            {
-                case Events.ClassifiedAdCreated e:
-                    _items.Add(new ReadModels.ClassifiedAdDetails
-                    {
-                        ClassifiedAdId = e.Id,
-                        SellerId = e.OwnerId
-                    });
-                    break;
-                case Events.ClassifiedAdTitleChanged e:
-                    UpdateItem(e.Id, ad => ad.Title = e.Title);
-                    break;
-                case Events.ClassifiedAdTextUpdated e:
-                    UpdateItem(e.Id, ad => ad.Description = e.AdText);
-                    break;
-                case Events.ClassifiedAdPriceUpdated e:
-                    UpdateItem(e.Id, ad =>
-                    {
-                        ad.Price = e.Price;
-                        ad.CurrencyCode = e.CurrencyCode;
-                    });
-                    break;
-                case Domain.UserProfile.Events.UserDisplayNameUpdated e:
-                    UpdateMultipleItems(x => x.SellerId == e.UserId,
-                        x => x.SellersDisplayName = e.DisplayName);
-                    break;
-            }
-
+            // TODO
             return Task.CompletedTask;
-        }
-
-        private void UpdateItem(Guid id, Action<ReadModels.ClassifiedAdDetails> update)
-        {
-            var item = _items.FirstOrDefault(x => x.ClassifiedAdId == id);
-            if (item == null)
-                return;
-
-            update(item);
-        }
-
-        private void UpdateMultipleItems(
-            Func<ReadModels.ClassifiedAdDetails, bool> query, 
-            Action<ReadModels.ClassifiedAdDetails> update)
-        {
-            foreach (var item in _items.Where(query))
-                update(item);
         }
 
         public void Stop() => _subscription.Stop();
